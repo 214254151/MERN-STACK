ec3ed78010234c45cfa1d8e7e05043eefec486bb
Abongile Tshopi <abongiletshopi@gmail.com>
2024-08-23 14:52:47 +0200

    making the env cifigurtion a private viriable

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require ('express')
 
 // express app
@@ -8,7 +10,8 @@ app.get('/', (req, res) => {
     res.json({msg: "Hello World"})
 })
 
-// listen for a port number request
+
+// listen for a port number request.
 app.listen(process.env.PORT, () => {
     console.log("listening on port", process.env.PORT) 
 })
