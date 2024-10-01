7e2cb6e644b514fe90fde2a2be26b78cc52abd40
Abongile Tshopi <abongiletshopi@gmail.com>
2024-10-01 11:35:33 +0200

    impord and extened middleware to use the json file for requests

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 require('dotenv').config();
 
 const express = require('express');
+const workoutRoutes = require(('./routes/workouts'));
 
 // express app creation
 const app = express();
 
-// port number and hiding it in the enc file
-// const PORT = process.env.PORT || 5000;
 
-// Routes:  handling requests
-app.get('/', (req, res) => {
-  res.json({mssg: 'Hello World!'});
-});
+// middleware app
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+
+
+// Routes:  handling requests for testing
+// `app.get('/', (req, res) => {
+//   res.json({mssg: 'Hello World!'});
+// });`
+
+
+// Routes
+app.use('/api/workouts', workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port 5000');
