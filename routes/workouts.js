d97d8a66d747677771fd761a9e9252e6515929c5
Abongile Tshopi <abongiletshopi@gmail.com>
2024-10-02 15:49:12 +0200

    commenteout server outside the backened

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,32 +1,32 @@
-const express = require('express');
+// const express = require('express');
 
-const router = express.Router();
+// const router = express.Router();
 
-// TEstiing for getting all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'GEt all the workouts'})
-})
+// // TEstiing for getting all workouts
+// router.get('/', (req, res) => {
+//     res.json({mssg: 'GEt all the workouts'})
+// })
 
-// GEt a single workout
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single workout'})
-})
-
-
-// POST a request
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST  new request/workout'})
-})
-
-// Delete a request
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a request/workout'})
-})
-
-// UPDATE a request
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE request/workout'})
-})
+// // GEt a single workout
+// router.get('/:id', (req, res) => {
+//     res.json({mssg: 'GET a single workout'})
+// })
 
 
-module.exports = router;
+// // POST a request
+// router.post('/', (req, res) => {
+//     res.json({mssg: 'POST  new request/workout'})
+// })
+
+// // Delete a request
+// router.delete('/:id', (req, res) => {
+//     res.json({mssg: 'DELETE a request/workout'})
+// })
+
+// // UPDATE a request
+// router.patch('/:id', (req, res) => {
+//     res.json({mssg: 'UPDATE request/workout'})
+// })
+
+
+// module.exports = router;
