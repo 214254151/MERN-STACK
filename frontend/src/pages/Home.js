94a781859015af5b1071bd8158ae4fd5b87fc251
Abongile Tshopi <abongiletshopi@gmail.com>
2024-10-10 10:57:46 +0200

    added Home.js, NavBar and other files in frontend

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+// useEffect
+
+const Home = () =>{
+    return(
+        <div className="home">
+           <h2>Welcome Home</h2> 
+        </div>
+    )
+}
+
+export default Home;
